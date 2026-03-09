342d6369c2cfa66b9546c08e4ea006a9d56d75ab
Nawin chander <nawinnapzy289@gmail.com>
2026-03-09 21:07:11 +0530

    add code

--- DSA/Space Complexity/space.js
+++ DSA/Space Complexity/space.js
@@ -40,6 +40,57 @@ console.log(createArray(5));
 
 // Space Complexity: O(n)
 
+///// O(n²) – Quadratic Space ///  n X n
+
+function createMatrix(n) {
+    let matrix = [];
+
+    for (let i = 0; i < n; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < n; j++) {
+            matrix[i][j] = i + j;
+        }
+    }
+
+    return matrix;
+}
+
+console.log(createMatrix(3));
+
+
+/// output
+
+[
+ [0,1,2],
+ [1,2,3],
+ [2,3,4]
+]
+
+/// If n = 100, matrix size = 100 × 100
+/// Space Complexity: O(n²)
+
+/// Recursive Space Complexity
+
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+
+/// OP
+
+factorial(5)
+factorial(4)
+factorial(3)
+factorial(2)
+factorial(1)
+
+// Each call uses memory.
+// If n = 5 → 5 stack frames.
+// Space Complexity: O(n)
+
+
 
 
 
