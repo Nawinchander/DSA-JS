b81ed6cea6f55fb727ef415e70210921f3b6c779
Nawin chander <nawinnapzy289@gmail.com>
2026-03-09 21:03:55 +0530

    add code

---
+++ DSA/Space Complexity/space.js
@@ -0,0 +1,25 @@
+///// O(1) – Constant Space
+
+function sum(a, b) {
+    let result = a + b;
+    return result;
+}
+
+console.log(sum(5, 10));
+
+// Variables used:
+
+// a
+// b
+// result
+
+// Even if numbers become larger, memory usage stays constant.
+
+// Space Complexity: O(1)
+
+
+
+
+
+
+
