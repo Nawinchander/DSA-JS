7efe01b42154266f03161d75ddc0047782454ade
Nawin chander <nawinnapzy289@gmail.com>
2026-03-10 22:36:28 +0530

    add the code

---
+++ DSA/Heap/Advance Heap/script.js
@@ -0,0 +1,19 @@
+
+// Find the Top K largest elements from an array.
+
+// Use a Min Heap of size K.
+
+// Keep only K largest numbers
+
+// If heap size > k → remove smallest
+
+// Input:
+// arr = [3, 2, 1, 5, 6, 4]
+// k = 2
+
+// Output:
+// [6,5]
+
+
+
+
